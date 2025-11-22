cac02bc19d04b1d099b2b55d26721878e993b98b
Jorge Martinez <jorgelmr01@hotmail.com>
2025-11-21 18:38:45 -0600

    Remove node demepndency

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [react()],
+    plugins: [react(), viteSingleFile()],
+    build: {
+        target: 'esnext',
+        assetsInlineLimit: 100000000,
+        chunkSizeWarningLimit: 100000000,
+        cssCodeSplit: false,
+        rollupOptions: {
+            output: {
+                inlineDynamicImports: true,
+                manualChunks: undefined,
+            },
+        },
+    },
 })
