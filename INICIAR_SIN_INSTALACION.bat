e4ce54ba7ddf771232db72b06320d8bd43e31ef5
Jorge Martinez <jorgelmr01@hotmail.com>
2025-11-21 18:43:42 -0600

    Include dist folder for zero-install distribution and update launcher for Chrome

--- INICIAR_SIN_INSTALACION.bat
+++ INICIAR_SIN_INSTALACION.bat
@@ -7,5 +7,16 @@ echo.
 echo Abriendo aplicacion en tu navegador...
 echo.
 
-:: Open index.html directly
-start "" "%~dp0dist\index.html"
+:: Try to find Chrome
+set "CHROME_PATH="
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" set "CHROME_PATH=C:\Program Files\Google\Chrome\Application\chrome.exe"
+if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" set "CHROME_PATH=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" set "CHROME_PATH=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
+
+if defined CHROME_PATH (
+    echo Abriendo en Google Chrome...
+    start "" "%CHROME_PATH%" "%~dp0dist\index.html"
+) else (
+    echo Chrome no encontrado, abriendo en navegador predeterminado...
+    start "" "%~dp0dist\index.html"
+)
